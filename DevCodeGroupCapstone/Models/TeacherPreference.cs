1d8cb53012ee7c44fde4260ac430f0b3c77789b6
alexHardy872 <alexhardy872@gmail.com>
2019-11-11 16:11:36 -0600

    added collums to teacherPreference model, starting work on controller to create preferences

--- DevCodeGroupCapstone/Models/TeacherPreference.cs
+++ DevCodeGroupCapstone/Models/TeacherPreference.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,24 @@ namespace DevCodeGroupCapstone.Models
 {
     public class TeacherPreference
     {
+        [Key]
+        public int TeacherPreferenceId { get; set; }
+
+        public TimeSpan defaultLessonLength { get; set; }
+
+        
+        public int maxMiles { get; set; }
+
+        public int maxMinutes { get; set; }
+
+        public decimal incementalCostPerMile { get; set; }
+
+        public decimal incementalCostPerMinute { get; set; }
+
+        [ForeignKey("Teacher")]
+        public int? teacherId { get; set; }
+        public Person Teacher { get; set; }
+
 
 
     }
