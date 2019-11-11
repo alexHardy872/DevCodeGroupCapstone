28de4e51dcae1f570ce882b4e22ed8905593352c
Adam Neujahr <adamneujahr@gmail.com>
2019-11-11 11:08:07 -0600

    Started teacher and lesson controller

---
+++ DevCodeGroupCapstone/Controllers/TeacherController.cs
@@ -0,0 +1,96 @@
+﻿using DevCodeGroupCapstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevCodeGroupCapstone.Controllers
+{
+    public class TeacherController : Controller
+    {
+        ApplicationDbContext context;
+
+        public TeacherController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Teacher/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Teacher/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teacher/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Teacher/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Teacher/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Teacher/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Teacher/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
