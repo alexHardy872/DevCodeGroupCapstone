ea34eb1ae97878ff9358d515582646e38a246d28
Adam Neujahr <adamneujahr@gmail.com>
2019-11-11 11:37:21 -0600

    Changed lesson model to include Length and Price for baseprice

---
+++ DevCodeGroupCapstone/Migrations/201911111735010_Lesson Model Change.cs
@@ -0,0 +1,22 @@
+﻿namespace DevCodeGroupCapstone.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class LessonModelChange : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Lessons", "Length", c => c.Int(nullable: false));
+            AddColumn("dbo.Lessons", "Price", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AlterColumn("dbo.Lessons", "cost", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Lessons", "cost", c => c.Double(nullable: false));
+            DropColumn("dbo.Lessons", "Price");
+            DropColumn("dbo.Lessons", "Length");
+        }
+    }
+}
