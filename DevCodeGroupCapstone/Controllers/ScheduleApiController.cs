caf81dff58772662110df186c8f375bd49b1f7bb
gabrielkunkel <henrysaltandson@gmail.com>
2019-11-12 14:28:43 -0600

    create schedule API Index

--- DevCodeGroupCapstone/Controllers/ScheduleApiController.cs
+++ DevCodeGroupCapstone/Controllers/ScheduleApiController.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 using DevCodeGroupCapstone.Models;
+
 
 namespace DevCodeGroupCapstone.Controllers
 {
@@ -19,36 +25,76 @@ namespace DevCodeGroupCapstone.Controllers
             context = ApplicationDbContext.Create();
         }
 
-        public async Task<IHttpActionResult> Index(string Id)
+        [System.Web.Http.HttpGet]
+        public async Task<IHttpActionResult> Index(string TeacherId, string beginningCalendarDate)
         {
+            DateTime beginningCalendarDateTime = DateTime.Parse(beginningCalendarDate); // use this for the date
+            int teacherIdInt = int.Parse(TeacherId);
+
+            List<Event> eventList = new List<Event>();
+
             try
             {
-                // todo: Index grab of events for Calendar
-                var person = await Task.Run(() => context.People
-                    .Include("avail")
-                    .Where(p => p.ApplicationId == Id)
-                    .SingleOrDefault()
+                var availabilities = await Task.Run(() => context.TeacherAvailabilities
+                    .Where(a => a.PersonId == teacherIdInt)
+                    .ToList()
                     );
 
-                TeacherAvail teacherAvail = new TeacherAvail();
+                var preferences = await Task.Run(() => context.Preferences
+                    .Where(p => p.teacherId == teacherIdInt)
+                    .SingleOrDefault()
+                );
 
-                PropertyInfo[] properties = typeof(TeacherAvail).GetProperties();
+                DateTime workingStartTime = availabilities[0].start;
+                double convertedLessonLength = Convert.ToDouble(preferences.defaultLessonLength);
+                TimeSpan timeSpanOfLesson = TimeSpan.FromMinutes(convertedLessonLength);
 
-                foreach (var item in person)
+                foreach (var availableTimeSpan in availabilities)
                 {
+                    DayOfWeek dayOfWeek = availableTimeSpan.weekDay; 
+                    DateTime workingDate = GetNextDayOfWeekForDateTime(dayOfWeek, beginningCalendarDateTime);
 
+                    while (workingStartTime + timeSpanOfLesson <= availableTimeSpan.end)
+                    {
+                        Event currentEvent = new Event();
+                        currentEvent.start = CombineDateAndTime(workingDate, workingStartTime);
+                        currentEvent.end = CombineDateAndTime(workingDate, workingStartTime + timeSpanOfLesson);
+                        eventList.Add(currentEvent);
+                    }
                 }
-
-
+                return Ok(eventList);
             }
             catch (Exception e)
             {
                 return InternalServerError(e);
             }
-
-
-            return Ok();
 ;        }
+
+        private DateTime GetNextDayOfWeekForDateTime(DayOfWeek dayOfWeek, DateTime dateTime)
+        {
+            while (dateTime.DayOfWeek != dayOfWeek)
+            {
+                dateTime = dateTime.AddDays(1);
+            }
+
+            return dateTime.Date; // time is zeroed out
+        }
+
+        private DateTime CombineDateAndTime(DateTime date, DateTime time)
+        {
+            return date + time.TimeOfDay;
+        }
+
+        private int GetNumberOfDaysForView(string view)
+        {
+            switch (view)
+            {
+                case "timeGridWeek":
+                    return 7;
+                default:
+                    return 0;
+            }
+        }
 
 
     }
---
+++ DevCodeGroupCapstone/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevCodeGroupCapstone.Models
+{
+    public class Event
+    {
+        public DateTime start { get; set; }
+
+        public DateTime end { get; set; }
+
+
+    }
+}
