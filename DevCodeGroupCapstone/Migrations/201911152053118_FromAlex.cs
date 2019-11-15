3ab1b4fd01400c6d9b9ee382996faaa3b5aaad80
trevor clements <trevortheamazing@gmail.com>
2019-11-15 15:21:07 -0600

    removed 'Edit' action option from TeacherAvailabilities.

---
+++ DevCodeGroupCapstone/Migrations/201911152053118_FromAlex.cs
@@ -0,0 +1,18 @@
+﻿namespace DevCodeGroupCapstone.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FromAlex : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Lessons", "requiresMakeup", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Lessons", "requiresMakeup");
+        }
+    }
+}
