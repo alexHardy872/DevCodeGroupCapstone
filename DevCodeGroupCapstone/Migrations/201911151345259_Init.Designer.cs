17d921bcf6d85932427fcb11f984986de9c2073c
Adam Neujahr <adamneujahr@gmail.com>
2019-11-15 08:39:31 -0600

    Added request lessons on StudentIndex

--- DevCodeGroupCapstone/Migrations/201911151345259_Init.Designer.cs
+++ DevCodeGroupCapstone/Migrations/201911151345259_Init.Designer.cs
@@ -13,7 +13,7 @@ namespace DevCodeGroupCapstone.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201911142154479_Init"; }
+            get { return "201911151345259_Init"; }
         }
         
         string IMigrationMetadata.Source
